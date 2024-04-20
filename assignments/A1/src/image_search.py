3f98f06e2d664a114a9f7d0ba0c99995ecba5af9
sabszh <sabrinazakihansen@hotmail.com>
2024-04-20 18:33:18 +0200

    updated sum

--- assignments/A1/src/image_search.py
+++ assignments/A1/src/image_search.py
@@ -8,8 +8,16 @@ import os
 import sys
 import cv2
 import numpy as np
+from numpy.linalg import norm
 import pandas as pd
 import argparse
+from tqdm import tqdm
+import tensorflow_hub as hub
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 sys.path.append(os.path.join(".."))
 
@@ -58,6 +66,42 @@ def compare_histograms(target_histogram, histograms_list):
                  for filename, histogram in histograms_list]
 
     return distances
+
+# Using VGG16 and KNN
+def extract_features(img_path, model):
+    """
+    Extract features from image data using pretrained model (e.g. VGG16)
+    """
+    input_shape = (224, 224, 3)
+    img = load_img(img_path, target_size=(input_shape[0], input_shape[1]))
+    img_array = img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+    features = model.predict(preprocessed_img, verbose=False)
+    flattened_features = features.flatten()
+    normalized_features = flattened_features / norm(features)
+    return normalized_features
+
+def find_similar_images(target_image_path, dataset_dir, num_neighbors=5):
+    model = VGG16(weights='imagenet', include_top=False, pooling='avg', input_shape=(224, 224, 3))
+
+    target_features = extract_features(target_image_path, model)
+
+    filenames = [os.path.join(dataset_dir, name) for name in sorted(os.listdir(dataset_dir))]
+
+    feature_list = []
+    for filename in tqdm(filenames, desc="Extracting features"):
+        feature_list.append(extract_features(filename, model))
+
+    neighbors = NearestNeighbors(n_neighbors=num_neighbors + 1, algorithm='brute', metric='cosine').fit(feature_list)
+
+    distances, indices = neighbors.kneighbors([target_features])
+
+    similar_images = []
+    for i in range(1, num_neighbors + 1):
+        similar_images.append(filenames[indices[0][i]])
+
+    return similar_images
 
 # Function to save results to CSV
 def save_to_csv(data, filename):
